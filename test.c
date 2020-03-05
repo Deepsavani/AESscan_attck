39d4352ae49df341e3997cf7bbab3a64556623bf
Archit <archit010@gmail.com>
2020-03-04 21:20:57 -0500

    Update test.c

--- test.c
+++ test.c
@@ -11,7 +11,6 @@
 
 #include "aes.h"
 #include "aes.c"
-#include "hextobin.c"
 
 typedef uint8_t state_t[4][4];
 
@@ -56,35 +55,12 @@ static void phex(uint8_t* str)
         printf("%.2x", str[i]);
     printf("\n");
 }
-char* itoa(int value, char* result, int base) 
-{
-		// check that the base if valid
-		if (base < 2 || base > 36) { *result = '\0'; return result; }
 
-		char* ptr = result, *ptr1 = result, tmp_char;
-		int tmp_value;
-
-		do {
-			tmp_value = value;
-			value /= base;
-			*ptr++ = "zyxwvutsrqponmlkjihgfedcba9876543210123456789abcdefghijklmnopqrstuvwxyz" [35 + (tmp_value - value * base)];
-		} while ( value );
-
-		// Apply negative sign
-		if (tmp_value < 0) *ptr++ = '-';
-		*ptr-- = '\0';
-		while(ptr1 < ptr) {
-			tmp_char = *ptr;
-			*ptr--= *ptr1;
-			*ptr1++ = tmp_char;
-		}
-		return result;
-}
 static void test_encrypt_ecb_verbose(void)
 {
     // Example of more verbose verification
 
-    uint8_t i,j,*wrap_b[16], b[2];
+    uint8_t i,j,wrap_b[16][2], b[2];
     
     
     // 128bit key
@@ -93,100 +69,130 @@ static void test_encrypt_ecb_verbose(void)
     struct AES_ctx ctx;
         AES_init_ctx(&ctx, key);
 
-    uint8_t temp[32] = {  (uint8_t) 0x00, (uint8_t) 0x00, (uint8_t) 0x00, (uint8_t) 0x00,
-                                (uint8_t) 0x00, (uint8_t) 0x00, (uint8_t) 0x00, (uint8_t) 0x00,
-                                (uint8_t) 0x00, (uint8_t) 0x00, (uint8_t) 0x00, (uint8_t) 0x00,
-                                (uint8_t) 0x00, (uint8_t) 0x00, (uint8_t) 0x00, (uint8_t) 0x00};
+    // uint8_t temp[32] = {        (uint8_t) 0x00, (uint8_t) 0x00, (uint8_t) 0x00, (uint8_t) 0x00,
+    //                             (uint8_t) 0x00, (uint8_t) 0x00, (uint8_t) 0x00, (uint8_t) 0x00,
+    //                             (uint8_t) 0x00, (uint8_t) 0x00, (uint8_t) 0x00, (uint8_t) 0x00,
+    //                             (uint8_t) 0x00, (uint8_t) 0x00, (uint8_t) 0x00, (uint8_t) 0x00};
 
     
 
-uint8_t a,x,y,m, plain_text[32];
-
+uint8_t a,x,y,m, plain_text[32],rk0[32];
+uint8_t encrypt_new[32],encrypt_previous[32],xor[32];
 for(x=(uint8_t)0; x<(uint8_t)16; x++){ 
+    uint8_t temp[32] = {        (uint8_t) 0x00, (uint8_t) 0x00, (uint8_t) 0x00, (uint8_t) 0x00,
+                                (uint8_t) 0x00, (uint8_t) 0x00, (uint8_t) 0x00, (uint8_t) 0x00,
+                                (uint8_t) 0x00, (uint8_t) 0x00, (uint8_t) 0x00, (uint8_t) 0x00,
+                                (uint8_t) 0x00, (uint8_t) 0x00, (uint8_t) 0x00, (uint8_t) 0x00};
+                                
     memcpy(plain_text, temp, 16);
-    for(y=(uint8_t)0; y<=(uint8_t)255; y++){
-        AES_ECB_encrypt(&ctx, plain_text + (i * 16));
-        printf("plain_text\n");
-        phex(plain_text);
-        uint8_t encrypt_new[32],encrypt_previous[32],xor[32];
-        unsigned char xorstring[32];
-        memcpy(encrypt_previous, plain_text, 16);
+    for(y=(uint8_t)0; y<=(uint8_t)254; y++){
+        AES_init_ctx(&ctx, key);
         a=0; 
-        long dec=0;
+        printf("OLD Input text : \n ");
+        phex(plain_text);
+        AES_ECB_encrypt_scan(&ctx, plain_text);
+        memcpy(encrypt_previous, plain_text, 16);
+        temp[x]=temp[x]+(0x01);
+        //abc[x] = temp[x]+(0x02);
+        memcpy(plain_text, temp, 16);
+        printf(" NEW Input text : \n ");
+        phex(plain_text);
+        AES_ECB_encrypt_scan(&ctx, plain_text);
+        memcpy(encrypt_new, plain_text, 16);
         temp[x]=temp[x]+(0x01);
         memcpy(plain_text, temp, 16);
-        AES_ECB_encrypt(&ctx, plain_text + (i * 16));
-        memcpy(encrypt_new, plain_text, 16);
-
-        //printf("encrypt previous=\n");
-        //phex(encrypt_previous);
-        //printf("encrypt new=\n");
-        //phex(encrypt_new);
-        //memcpy(xor, (int)(encrypt_previous^encrypt_new), 16);
-        //uint8_t xor = (encrypt_previous^encrypt_new);
+        printf("encrypt new=\n");
+        phex(encrypt_new);
+        printf("encrypt previous=\n");
+        phex(encrypt_previous);
         for(i=0;i<16;i++)
         {
             xor[i]=encrypt_new[i]^encrypt_previous[i];
-            //printf("\nelement=")
         }
-
-        //uint8_t *xor1=&xor;
-        uint8_t change[16];
-        printf("xor=\n");
+        printf("Ye XOR hai\n");
         phex(xor);
 
-        // xor[33]='\0';
-        for(i=0; i<32; i++){
-            xor[i] >>= 1;
-            printf("%.2x\n", xor[i]);
+        for(i=0; i<16; i++){
+            for(j=0;j<8;j++){
+            if(xor[i]&1){
+                a++;
+            }
+            xor[i]>>=1;
+            
         }
-
         }
-        printf("Number of 1's :: %d\n\n", a);
-        
+        printf("\nNumber of ones = ");
+        printf("%d\n",a);
         if(a==9){
-            b[0]=226;
-            b[1]=227;
-            wrap_b[x] = b;
-            printf("%s", *wrap_b);
-            printf(".....d.....");
+            wrap_b[x][0] = 226;
+            wrap_b[x][1] = 227;
+            printf("\n a value = %.2x\n",encrypt_new[x]);
+            rk0[x]=(encrypt_new[x])^(0xE3);
+            //printf("%s", *wrap_b);
+            printf("\n------------------------break -----------------------\n");
             break;
         }
         else if(a==12){
-            b[0]=242;
-            b[1]=243;
-            wrap_b[x] = b;
-            printf("%s", *wrap_b);
-            printf("......c....");
+            wrap_b[x][0] = 242;
+            wrap_b[x][1] = 243;
+            printf("\n a value = %.2x\n",encrypt_new[x]);
+            rk0[x]=(encrypt_new[x])^(0xF3);
+            //printf("%s", *wrap_b);
+            printf("\n------------------------break -----------------------\n");
             break;
         }
         else if(a==23){
-            b[0]=122;
-            b[1]=123;
-            wrap_b[x] = b;
-            printf("%s", *wrap_b);
-            printf(".........b.");
+            wrap_b[x][0] = 122;
+            wrap_b[x][1] = 123;
+            printf("\n a value = %.2x\n",encrypt_new[x]);
+            rk0[x]=(encrypt_new[x])^(0x7B);
+            //printf("%s", *wrap_b);
+            printf("\n------------------------break -----------------------\n");
             break;
         }
         else if(a==24){
-            b[0]=130;
-            b[1]=131;
-            wrap_b[x] = b;
+            wrap_b[x][0] = 130;
+            wrap_b[x][1] = 131;
+            printf("\n a value = %.2x\n",encrypt_new[x]);
+            rk0[x]=(encrypt_new[x])^(0x83);
             printf("%s", *wrap_b);
-            printf("..........a");
+            printf("\n------------------------break -----------------------\n");
             break;
             }
         else{
-            break;
+            // temp[x]=temp[x]+(0x01);
+            // memcpy(plain_text, temp, 16);
+            continue;
         }
     }
 
-    //continue;
+    continue;
 }
 
-// for(i=0;i<16;i++){
-//     printf("%d", (*wrap_b)[i]);
-// }
+printf("rk0 ------\n");
+phex(rk0);
+
+//  for(i=0;i<16;i++){
+//      for(j=0;j<2;j++){
+//      printf("%d ", wrap_b[i][j]);
+//      if(j==1){
+//          printf("\n");
+//      }
+//     }
+//  }
+
+//  for(i=0;i<16;i++){
+//      uint8_t temp[32] = {       (uint8_t) 0x0d, (uint8_t) 0x09, (uint8_t) 0x77, (uint8_t) 0x14,
+//                                 (uint8_t) 0x03, (uint8_t) 0xc0, (uint8_t) 0xae, (uint8_t) 0x00,
+//                                 (uint8_t) 0x98, (uint8_t) 0x1f, (uint8_t) 0x97, (uint8_t) 0x00,
+//                                 (uint8_t) 0x29, (uint8_t) 0x16, (uint8_t) 0x00, (uint8_t) 0xd0}; 
+//      temp[x]=temp[x]+0x05;
+//      AES_ECB_encrypt(&ctx, temp);
+//      encrypt_round1[x] = (*temp)[x];
+//  }
+//  printf("\nValue here is this: ");
+//  phex(encrypt_round1);
+ 
 
     // for(i=0; i<len(plain_text); i+=1){
     //     uint8_t new_text[64] = plain_text + 1;                         
@@ -225,6 +231,4 @@ for(x=(uint8_t)0; x<(uint8_t)16; x++){
     //     printf("%.2x", e.arr[k][l]);
     //   }
     // }
-
-
-
+}
